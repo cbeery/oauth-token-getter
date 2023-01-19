c446dde86c73e91670be8dcf83808f32d6465858
affycurt <cbeery@affygility.com>
2023-01-19 11:34:02 -0700

    Removed dup constant declaration for DROPBOX_CLIENT_ID

--- providers/dropbox_cb_affy.rb
+++ providers/dropbox_cb_affy.rb
@@ -2,18 +2,18 @@
 
 # http://localhost:9292/dropbox_cb_affy/callback
 
-DROPBOX_CLIENT_ID = ENV['DROPBOX_CB_AFFY_CLIENT_ID']
-DROPBOX_CLIENT_SECRET = ENV['DROPBOX_CB_AFFY_CLIENT_SECRET']
+DROPBOX_CB_AFFY_CLIENT_ID = ENV['DROPBOX_CB_AFFY_CLIENT_ID']
+DROPBOX_CB_AFFY_CLIENT_SECRET = ENV['DROPBOX_CB_AFFY_CLIENT_SECRET']
 ACCOUNT_ID = ENV['DROPBOX_CB_AFFY_ACCOUNT_ID']
 TEAM_MEMBER_ID = ENV['DROPBOX_CB_AFFY_TEAM_MEMBER_ID']
 
 get '/dropbox_cb_affy/authorize' do
-	url = "https://www.dropbox.com/oauth2/authorize?client_id=#{DROPBOX_CLIENT_ID}&response_type=code&redirect_uri=#{url('/dropbox_cb_affy/callback')}&force_reapprove=false"
+	url = "https://www.dropbox.com/oauth2/authorize?client_id=#{DROPBOX_CB_AFFY_CLIENT_ID}&response_type=code&redirect_uri=#{url('/dropbox_cb_affy/callback')}&force_reapprove=false"
 	redirect to(url)
 end
 
 get '/dropbox_cb_affy/callback' do
-	post_params = {client_id: DROPBOX_CLIENT_ID, client_secret: DROPBOX_CLIENT_SECRET, code: params[:code], grant_type: 'authorization_code', redirect_uri: url('/dropbox_cb_affy/callback')}
+	post_params = {client_id: DROPBOX_CB_AFFY_CLIENT_ID, client_secret: DROPBOX_CB_AFFY_CLIENT_SECRET, code: params[:code], grant_type: 'authorization_code', redirect_uri: url('/dropbox_cb_affy/callback')}
 
 	response = Net::HTTP.post_form(URI("https://api.dropboxapi.com/oauth2/token"), post_params)
 
